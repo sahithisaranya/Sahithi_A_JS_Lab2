359c431cd5fb85a2f1065df40efc9eeccabfc11f
sahithisaranya <sahithisaranya@gmail.com>
2023-01-14 19:51:26 +0530

    quiz app dev

--- quiz.js
+++ quiz.js
@@ -1,3 +1,4 @@
+//declare questions array
 const questions=[
     "JavaScript supports",
     "Which language is used for styling web pages?",
@@ -6,6 +7,7 @@ const questions=[
     "JavaScript is a"
 ];
 
+//declare 2d array for answers
 const answers=[
     ["Functions","XHTML","CSS","HTML"],
     ["HTML","JQuery","CSS","XML"],
@@ -14,7 +16,93 @@ const answers=[
     ["Language","Programming Language","Development","All"]
 ];
 
+//declare array for correct answers
+const correctAnswers=["Functions","CSS","Django","PHP","Programming Language"]
 
-function QuestionAndAnswer(){
 
+
+//function to initialize quiz objects
+const questionAndAnswer=function(questionArr,answersChoicesArr,correctAnswerArr){
+    this.questionArr=questionArr;
+    this.answersChoicesArr=answersChoicesArr;
+    this.correctAnswerArr=correctAnswerArr;
 }
+
+
+
+function QuizApplication(questionAndAnswer){
+
+    //variables to track score and page number
+    let score=0;
+    let pageNum=0;
+
+    this.questionAndAnswer=questionAndAnswer;
+
+    //initialize quiz
+    this.startQuiz=function(){
+        this.displayStartPage();
+    }
+
+    //function to display starting page.
+    //this sets score to 0 and page number to 0
+    //calls initial title, qa and progress sections
+    this.displayStartPage=function(){
+        score=0;
+        pageNum=0;
+        //this.displayTitleSection("JavaScript");
+        this.displayQASection();
+        this.addEventListeners();
+        this.displayProgessSection();
+    }
+
+    this.displayNextPage=function(){
+        pageNum++;
+        this.displayQASection();
+        this.addEventListeners();
+        this.displayProgessSection();
+    }
+    // this.displayTitleSection=function(titleStr){
+    //     this.titleStr=titleStr;
+    //     const titleHTMLElement=document.getElementById("quiz");
+    //     titleHTMLElement.innerHTML=`<h1>${this.titleStr}</h1>`;
+    // }
+
+    this.displayQASection=function(){
+        const questionHTMLElement=document.getElementById("question");
+        questionHTMLElement.innerHTML=this.questionAndAnswer.questionArr[pageNum];
+
+        for(let i=0;i<this.questionAndAnswer.answersChoicesArr[pageNum].length;i++){
+            const answerChoiceHTMLElement=document.getElementById("choice"+i);
+            answerChoiceHTMLElement.innerHTML=this.questionAndAnswer.answersChoicesArr[pageNum][i];
+        }
+    }
+
+    this.displayProgessSection=function(){
+        const progressHTMLElement=document.getElementById("progress");
+        progressHTMLElement.innerHTML=`Question ${pageNum+1} of ${questions.length}`;
+    }
+
+    this.addEventListeners=function(){
+        const qaObj=this.questionAndAnswer;
+        for(let i=0;i<qaObj.answersChoicesArr[pageNum].length;i++){
+            const answerBtnHTMLElement=document.getElementById("btn"+i);
+            answerBtnHTMLElement.onclick=function(event){
+                const currentTarget=event.currentTarget;
+                const selectedAns=currentTarget.children[0].innerHTML;
+                if(selectedAns===qaObj.correctAnswerArr[pageNum]){
+                    score++;
+                }
+
+                this.displayNextPage();
+            }
+        }
+        
+    }
+
+}   
+
+const quizApp=new QuizApplication(new questionAndAnswer(questions,answers,correctAnswers));
+
+quizApp.startQuiz();
+
+

6dc672ab4ecbb036ae64b02917b4cbfc0d93c2db
sahithisaranya <sahithisaranya@gmail.com>
2023-01-14 10:06:27 +0530

    Adding arrays

--- quiz.js
+++ quiz.js
@@ -1,1 +1,20 @@
-const name="Hello world!"
+const questions=[
+    "JavaScript supports",
+    "Which language is used for styling web pages?",
+    "Which is not a JavaScript Framework?",
+    "Which is used to connect to database?",
+    "JavaScript is a"
+];
+
+const answers=[
+    ["Functions","XHTML","CSS","HTML"],
+    ["HTML","JQuery","CSS","XML"],
+    ["Python Script","JQuery","Django","NodeJS"],
+    ["PHP","HTML","JS","All"],
+    ["Language","Programming Language","Development","All"]
+];
+
+
+function QuestionAndAnswer(){
+
+}
